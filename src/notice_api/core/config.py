a3f4ebd82f314f0c9efb44fbded93e59ae708305
alan910127 <70696274+alan910127@users.noreply.github.com>
2023-12-16 16:32:44 +0800

    🦺 ignore extra entries in .env files

--- src/notice_api/core/config.py
+++ src/notice_api/core/config.py
@@ -5,9 +5,11 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    """Settings for the Not!ce API."""
-
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        extra="ignore",
+    )
 
     API_V1_STR: str = "/api/v1"
     """API version 1 prefix."""
