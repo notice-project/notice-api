c8cc5abdbf8846f18aade62af65be4722a13ac7b
alan910127 <70696274+alan910127@users.noreply.github.com>
2023-12-17 18:03:12 +0800

    ✨ add live transcription page

--- src/notice_api/main.py
+++ src/notice_api/main.py
@@ -1,8 +1,10 @@
 import importlib.metadata
 
+import structlog
 from asgi_correlation_id import CorrelationIdMiddleware
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import HTMLResponse
 from pydantic import BaseModel
 from starlette.middleware.sessions import SessionMiddleware
 
@@ -45,6 +47,74 @@ def health_check() -> HealthCheckResponse:
     """Check if the server is running."""
 
     return HealthCheckResponse()
+
+
+MEDIA_RECORDER_INTERVAL = 1000
+html = f"""
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Live Transcription</title>
+    </head>
+    <body>
+        <h1>Live Transcription Test</h1>
+        <button id="record-btn">Start Recording</button>
+        <p id="transcript"></p>
+        <script>
+            var recordBtn = document.querySelector('#record-btn');
+            var isRecording = false;
+            var ws = new WebSocket("ws://localhost:8000/transcription");
+            var mediaRecorder;
+            navigator.mediaDevices
+                .getUserMedia({{ audio: true }})
+                .then(stream => {{
+                    console.log(stream);
+                    mediaRecorder = new MediaRecorder(stream);
+                    console.log(mediaRecorder)
+                    mediaRecorder.ondataavailable = (e) => {{
+                        console.log(e);
+                        ws.send(e.data);
+                    }}
+                }});
+            recordBtn.addEventListener('click', () => {{
+                console.log('clicked');
+                if (isRecording) {{
+                    recordBtn.innerHTML = 'Start Recording';
+                    mediaRecorder.stop();
+                    isRecording = false;
+                }} else {{
+                    recordBtn.innerHTML = 'Recording...';
+                    mediaRecorder.start({MEDIA_RECORDER_INTERVAL});
+                    isRecording = true;
+                }}
+            }});
+            ws.onmessage = function(event) {{
+                console.log(event);
+                var transcript = document.getElementById('transcript');
+                transcript.innerHTML += event.data;
+            }};
+        </script>
+    </body>
+</html>
+"""
+
+
+@app.get("/transcription")
+def live_transcription_page():
+    return HTMLResponse(html)
+
+
+@app.websocket("/transcription")
+async def live_transcription(ws: WebSocket) -> HealthCheckResponse:
+    """WebSocket endpoint for live audio transcription."""
+
+    logger = structlog.get_logger("ws")
+    await ws.accept()
+    logger.info("Websocket connection established")
+    while True:
+        _data = await ws.receive()
+        logger.info("Received data")
+        await ws.send_text("DATA RECEIVED\n")
 
 
 app.include_router(auth_router)
