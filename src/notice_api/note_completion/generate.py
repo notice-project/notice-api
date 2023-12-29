0ad7b89f0a8d72b1eb016a0b5d6880363ab805c9
alan910127 <70696274+alan910127@users.noreply.github.com>
2023-12-29 14:18:08 +0800

    ♻️ move out local testing code

--- src/notice_api/note_completion/generate.py
+++ src/notice_api/note_completion/generate.py
@@ -1,26 +1,25 @@
 import string
+from pathlib import Path
+from typing import Sequence
 
-from . import model
+from notice_api.note_completion import model
 
 
-def generate_note(trancript: str, usernote: str) -> str:
-    # 如果想先用讀檔測試可以用下面這段
-
-    f = open("C:/Users/Homeuser/Desktop/HCI/notice-api/src/notice_api/ml_vs_dl.txt")
-    input = []
-    ALPHA = string.ascii_letters
-    for line in f.readlines():
-        if line.startswith(tuple(ALPHA)):
-            # print(line)
-            input.append(line)
-    f.close()
-
-    # 如果要直接把string傳進去用這個
-    input = trancript
-
-    outcome = model.gen_model(input, usernote)
+def generate_note(transcript: str | Sequence[str], usernote: str) -> str:
+    outcome = model.gen_model(transcript, usernote)
     return outcome
 
 
-# 現在第三條chain還沒弄好 我usernote先隨便傳值進去 之後才會用到這個參數
-print(generate_note("1", "2"))
+# for testing purpose
+def generate_note_from_file(file_path: Path | str, usernote: str) -> str:
+    transcript = Path(file_path).read_text().splitlines()
+    transcript = [
+        line for line in transcript if line.startswith(tuple(string.ascii_letters))
+    ]
+    outcome = generate_note(transcript, usernote)
+    return outcome
+
+
+if __name__ == "__main__":
+    notice_api_path = Path(__file__).resolve().parent.parent
+    generate_note_from_file(notice_api_path / "ml_vs_dl.txt", "test")
