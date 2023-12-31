3efcc7cef1cdec533febfd48812a5631900070e2
alan910127 <70696274+alan910127@users.noreply.github.com>
2023-12-31 16:58:23 +0800

    🏷️ add FastAPI notes endpoint representation models

--- src/notice_api/notes/schema.py
+++ src/notice_api/notes/schema.py
@@ -6,12 +6,44 @@ from sqlalchemy import func
 from sqlmodel import Field, SQLModel
 
 
-class Note(SQLModel, table=True):
+class NoteBase(SQLModel):
+    """Base model for a note."""
+
+    title: str
+
+
+class Note(NoteBase, table=True):
     """A note for the associated user."""
 
-    __tablename__ = "bookshelf"  # pyright: ignore[reportGeneralTypeIssues]
+    __tablename__ = "note"  # pyright: ignore[reportGeneralTypeIssues]
 
-    id: Optional[UUID] = Field(None, primary_key=True)
+    id: Optional[UUID] = Field(
+        default=None,
+        primary_key=True,
+        sa_column_kwargs={"default": func.uuid()},
+    )
     title: str
-    created_at: datetime = Field(sa_column_kwargs={"server_default": func.now()})
+    created_at: Optional[datetime] = Field(
+        default=None, sa_column_kwargs={"server_default": func.now()}
+    )
+    bookshelf_id: UUID = Field(foreign_key="bookshelf.id", index=True)
     user_id: str = Field(foreign_key="user.id", index=True)
+
+
+class NoteRead(NoteBase):
+    """Model for reading a note."""
+
+    id: UUID
+    created_at: datetime
+
+
+class NoteCreate(NoteBase):
+    """Model for creating a note."""
+
+    pass
+
+
+class NoteUpdate(SQLModel):
+    """Model for updating a note."""
+
+    title: Optional[str] = None
