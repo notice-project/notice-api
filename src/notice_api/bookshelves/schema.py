b6a63936c1bb5feece806db732b12fd34f064e43
alan910127 <70696274+alan910127@users.noreply.github.com>
2023-12-31 16:19:08 +0800

    🏷️ add models for FastAPI endpoint request/response

--- src/notice_api/bookshelves/schema.py
+++ src/notice_api/bookshelves/schema.py
@@ -2,8 +2,15 @@ from datetime import datetime
 from typing import Optional
 from uuid import UUID
 
+from pydantic import ConfigDict
 from sqlalchemy import func
 from sqlmodel import Field, SQLModel
+
+
+class BookshelfBase(SQLModel):
+    """Base model for a bookshelf."""
+
+    title: str
 
 
 class Bookshelf(BookshelfBase, table=True):
@@ -20,3 +27,24 @@ class Bookshelf(BookshelfBase, table=True):
         default=None, sa_column_kwargs={"server_default": func.now()}
     )
     user_id: str = Field(foreign_key="user.id", index=True)
+
+
+class BookshelfCreate(BookshelfBase):
+    """Model for creating a bookshelf."""
+
+    pass
+
+
+class BookshelfRead(BookshelfBase):
+    """Model for reading a bookshelf."""
+
+    model_config = ConfigDict(from_attributes=True)
+
+    id: UUID
+    created_at: datetime
+
+
+class BookshelfUpdate(SQLModel):
+    """Model for updating a bookshelf."""
+
+    title: Optional[str] = None
