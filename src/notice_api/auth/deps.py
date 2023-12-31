0a68f1161e4f5cb946d894623060424701919bfe
alan910127 <70696274+alan910127@users.noreply.github.com>
2023-12-31 14:58:42 +0800

    🛂 throw unauthorized in get_current_user dependency

--- src/notice_api/auth/deps.py
+++ src/notice_api/auth/deps.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from typing import Annotated, Optional
 
-from fastapi import Depends, Header
+from fastapi import Depends, Header, HTTPException, status
 from sqlmodel import select
 
 from notice_api.auth.schema import Session, User
@@ -11,12 +11,12 @@ from notice_api.db import AsyncSession, get_async_session
 async def get_current_user(
     db: Annotated[AsyncSession, Depends(get_async_session)],
     session_token: Annotated[Optional[str], Header(alias="X-Session-Token")] = None,
-) -> Optional[User]:
+) -> User:
     """Get the current user from the database.
 
     This function is used as a dependency for FastAPI endpoints. It will return
     the current user if the session token is valid and the session has not
-    expired. Otherwise, it will return None.
+    expired. Otherwise, it will raise an HTTPException with status code 401.
     """
 
     statement = (
@@ -28,6 +28,9 @@ async def get_current_user(
     result = await db.exec(statement)
     data = result.first()
     if data is None:
-        return None
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="You are not logged in.",
+        )
 
     return data[1]
