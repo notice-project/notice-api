07390d769a3639f18af53ccf0aa0e3a11995d902
Li-Lun Lin <70696274+alan910127@users.noreply.github.com>
2023-12-31 16:22:01 +0800

    🐛 fix multiple declaration of Note

--- src/notice_api/notes/schema.py
+++ src/notice_api/notes/schema.py
@@ -15,16 +15,3 @@ class Note(SQLModel, table=True):
     title: str
     created_at: datetime = Field(sa_column_kwargs={"server_default": func.now()})
     user_id: str = Field(foreign_key="user.id", index=True)
-
-
-class Note(SQLModel, table=True):
-    """A note for the associated user."""
-
-    __tablename__ = "note"  # pyright: ignore[reportGeneralTypeIssues]
-
-    id: Optional[UUID] = Field(None, primary_key=True)
-    title: str
-    content: str
-    created_at: datetime = Field(sa_column_kwargs={"server_default": func.now()})
-    bookshelf_id: UUID = Field(foreign_key="bookshelf.id", index=True)
-    user_id: str = Field(foreign_key="user.id", index=True)
